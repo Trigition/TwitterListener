b43d6b9d76be7de9538aeaaad08a1a0fa78af6bc
unknown <trigition@gmail.com>
2015-05-06 17:16:46 -0600

    Working on implementing Text to Speech using pyttx

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+__author__ = 'William'
+
+import pyttsx
+
+engine = pyttsx.init()
+engine.setProperty('rate', 70)
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    print "Using voice:", voice.id
+    engine.setProperty('voice', voice.id)
+    engine.say("Hi there, how's you ?")
+engine.runAndWait()
